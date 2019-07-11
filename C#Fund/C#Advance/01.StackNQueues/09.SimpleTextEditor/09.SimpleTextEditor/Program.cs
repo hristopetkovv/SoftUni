eda3cdbca9fd63d77613378f895d117e1fa0ebce
Hristo <i4kowee@gmail.com>
2019-07-11 19:11:53 +0300

    Added more exercises

---
+++ C#Fund/C#Advance/01.StackNQueues/09.SimpleTextEditor/09.SimpleTextEditor/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _09.SimpleTextEditor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Stack<string> stackOfText = new Stack<string>();
+            StringBuilder text = new StringBuilder();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string command = input[0];
+
+                if(command == "1")
+                {
+                    stackOfText.Push(text.ToString());
+                    text.Append(input[1]);
+                }
+                else if(command == "2")
+                {
+                    int index = int.Parse(input[1]);
+                    stackOfText.Push(text.ToString());
+                    text.Remove(text.Length - index, index);
+                }
+                else if(command == "3")
+                {
+                    int index = int.Parse(input[1]);
+                    Console.WriteLine(text[index - 1]);
+                }
+                else if(command == "4")
+                {
+                    text.Clear();
+                    text.Append(stackOfText.Pop());
+                }
+            }
+        }
+    }
+}
