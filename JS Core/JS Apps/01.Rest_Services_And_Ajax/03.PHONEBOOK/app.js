6171b010084e2b9eac4909bcdd175c2f7379c6b3
Hristo <i4kowee@gmail.com>
2019-07-16 18:49:45 +0300

    More exercises

--- JS Core/JS Apps/01.Rest_Services_And_Ajax/03.PHONEBOOK/app.js
+++ JS Core/JS Apps/01.Rest_Services_And_Ajax/03.PHONEBOOK/app.js
@@ -1,29 +1,97 @@
 function attachEvents() {
-    document.querySelector("#btnLoad")
-        .addEventListener('click', function() {
-        let url = 'https://phonebook-nakov.firebaseio.com/phonebook.json';
+    clearPhoneBook();
+
+    const loadButton = document.getElementById('btnLoad');
+    loadButton.addEventListener('click', loadContacts);
+
+    const createButton = document.getElementById('btnCreate');
+    createButton.addEventListener('click', createContact);
+
+    const url = `https://phonebook-nakov.firebaseio.com/phonebook.json`;
+
+    function loadContacts() {
+        clearPhoneBook();
 
         fetch(url)
-            .then((request) => request.json())
-            .then((data) => {
-                let values = Object.values(data);
+            .then(request => request.json())
+            .then(data => {
+                const contacts = Object.values(data);
 
-                for(let value of values) {
-                    let name = value.person;
-                    let phoneNumber = value.phone;
-                    
-                    let delBtn = document.createElement('button');
-                    delBtn.textContent = 'DELETE'; 
+                for (const contact of contacts) {
+                    const name = contact.person;
+                    const phoneNumber = contact.phone;
 
-                    let listItem = document.createElement('li');
-                    listItem.textContent = `${name}: ${phoneNumber}`;
+                    const deleteButton = document.createElement('button');
+                    deleteButton.textContent = 'Delete';
 
-                    listItem.appendChild(delBtn);
-                    
-                    document.querySelector('#phonebook li').appendChild(delBtn);
+                    const li = document.createElement('li');
+                    li.textContent = `${name}: ${phoneNumber} `;
+                    li.appendChild(deleteButton);
+
+                    document.getElementById('phonebook').appendChild(li);
+
+                    deleteButton.addEventListener('click', (e) => {
+                        const currentContact = e.target.parentNode;
+                        const contactInfo = currentContact.textContent
+                            .split(' ').filter(x => x !== '');
+
+                        const name = contactInfo[0];
+                        const number = contactInfo[1];
+                        let searchedId = '';
+
+                        const contactsIds = Object.keys(data);
+
+                        for (const id of contactsIds) {
+                            if (data[id].person === name
+                                && data[id].phone === number) {
+                                searchedId = id;
+                                break;
+                            }
+                        }
+
+                        currentContact.remove();
+
+                        const idUrl = 'https://phonebook-nakov.firebaseio.com/phonebook'
+                        + '/' + searchedId + '.json';
+
+                        fetch(idUrl, {
+                            method: 'delete',
+                            body: JSON.stringify(searchedId)
+                        })
+                            .then(resolve => resolve.json());
+                    });
                 }
+            });
+    }
+
+    function createContact() {
+        clearPhoneBook();
+
+        const name = document.getElementById('person');
+        const number = document.getElementById('phone');
+
+        if (name.value && number.value) {
+            let currentPerson = {
+                person: name.value,
+                phone: number.value
+            };
+
+            fetch(url, {
+                method: 'post',
+                body: JSON.stringify(currentPerson)
             })
-    })
+                .then(res => res.json());
+        }
+
+        loadContacts();
+
+        name.value = '';
+        number.value = '';
+    }
+}
+
+function clearPhoneBook() {
+    document.getElementById('phonebook').innerHTML = '';
 }
 
 attachEvents();
--- JS Core/JS Apps/01.Rest_Services_And_Ajax/04.MESSENGER/app.js
+++ JS Core/JS Apps/01.Rest_Services_And_Ajax/04.MESSENGER/app.js
@@ -1,5 +1,53 @@
 function attachEvents() {
-    console.log('TODO...');
+    const url = 'https://rest-messanger.firebaseio.com/messanger.json';
+    const messages = document.querySelector("#messages");
+    const inputAuthor = document.querySelector("#author");
+    const inputContent = document.querySelector("#content");
+
+    const sendButton = document.querySelector("#submit");
+    const refreshButton = document.querySelector("#refresh");
+
+    sendButton.addEventListener('click', sendMessage);
+    refreshButton.addEventListener('click', refreshMessages);
+
+    function sendMessage() {
+        const author = inputAuthor.value;
+        const content = inputContent.value;
+
+        if(author && content) {
+            const message = {
+                author,
+                content
+            };
+
+            messages.textContent += `${author}: ${content}\n`;
+
+            fetch(url, {
+                method: 'post',
+                body: JSON.stringify(message)
+            })
+                .then(resolve => resolve.json());
+        }
+
+        inputAuthor.value = '';
+        inputContent.value = '';
+    }
+
+    function refreshMessages() {
+        messages.textContent = '';
+
+        fetch(url)
+            .then(request => request.json())
+            .then(data => {
+                const currentMessages = Object.values(data);
+
+                for(const message of currentMessages) {
+                    const author = message.author;
+                    const content = message.content;
+                    messages.textContent += `${author}: ${content}\n`;
+                }
+            });
+    }
 }
 
 attachEvents();
