6278f9acecc275edf9d15f613950e86318e3cb62
Hristo <i4kowee@gmail.com>
2019-08-06 19:12:36 +0300

    Added exercises

---
+++ Tech Module/MidExam/02.NumberArray/02.NumberArray/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.NumberArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToList();
+
+            string input = Console.ReadLine();
+            while (input != "End")
+            {
+                string command = input.Split(" ")[0];
+                if (command == "Switch")
+                {
+                    int index = int.Parse(input.Split(" ")[1]);
+                    if (index >= 0 && index < list.Count)
+                    {
+                        int number = list.ElementAt(index);
+                        list.RemoveAt(index);
+                        int newNumber = 0;
+                        newNumber = number * -1;
+                        list.Insert(index, newNumber);
+                    }
+                }
+
+                else if (command == "Change")
+                {
+                    int index = int.Parse(input.Split(" ")[1]);
+                    int value = int.Parse(input.Split(" ")[2]);
+
+                    list.RemoveAt(index);
+                    list.Insert(index, value);
+                }
+
+                else if (command == "Sum")
+                {
+                    string param = input.Split(" ")[1];
+                    if (param == "Positive")
+                    {
+                        int sumOfPositive = 0;
+                        for (int i = 0; i < list.Count; i++)
+                        {
+                            if (list[i] >= 0)
+                            {
+                                sumOfPositive += list[i];
+                            }
+                        }
+                        Console.WriteLine(sumOfPositive);
+                    }
+                    else if (param == "Negative")
+                    {
+                        int sumOfNegative = 0;
+                        for (int i = 0; i < list.Count; i++)
+                        {
+                            if (list[i] < 0)
+                            {
+                                sumOfNegative += list[i];
+                            }
+                        }
+                        Console.WriteLine(sumOfNegative);
+                    }
+                    else if (param == "All")
+                    {
+                        int sum = 0;
+                        for (int i = 0; i < list.Count; i++)
+                        {
+                            sum += list[i];
+                        }
+                        Console.WriteLine(sum);
+                    }
+                }
+                input = Console.ReadLine();
+            }
+
+            List<int> positiveNumbers = new List<int>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] >= 0)
+                {
+                    positiveNumbers.Add(list[i]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", positiveNumbers));
+        }
+    }
+}
---
+++ Tech Module/MidExam/03.ContactList/03.ContactList/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.ContactList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = Console.ReadLine()
+                .Split(" ")
+                .Where(x => !string.IsNullOrWhiteSpace(x))
+                .ToList();
+
+            var print = new List<string>();
+
+            string input;
+
+            while (!(input = Console.ReadLine()).Contains("Print"))
+            {
+                string command = input.Split(" ")[0];
+
+                if (command == "Add")
+                {
+                    string contact = input.Split(" ")[1];
+                    int index = int.Parse(input.Split(" ")[2]);
+
+                    if(!list.Contains(contact))
+                    {
+                        list.Add(contact);
+                    }
+                    else if (index >= 0 && index < list.Count)
+                    {
+                        list.Insert(index, contact);
+                    }
+                }
+                if(command == "Remove")
+                {
+                    int index = int.Parse(input.Split(" ")[1]);
+                    if(index >= 0 && index < list.Count)
+                    {
+                        list.RemoveAt(index);
+                    }
+                }
+                if(command == "Export")
+                {
+                    int startIndex = int.Parse(input.Split(" ")[1]);
+                    int count = int.Parse(input.Split(" ")[2]);
+
+                    if(startIndex >= 0 && startIndex < list.Count && count > 0)
+                    {
+                        if(count >= list.Count || count <= 0)
+                        {
+                            count = list.Count;
+                        }
+                        print = list.Skip(startIndex).Take(count).Select(x => x).ToList();
+
+                        Console.WriteLine(string.Join(" ", print));
+                        print.Clear();
+                    }
+                }
+
+            }
+            if(input.Split(" ")[1] == "Reversed")
+            {
+                list.Reverse();
+            }
+            Console.WriteLine($"Contacts: {string.Join(" ", list)}");
+        }
+    }
+}
