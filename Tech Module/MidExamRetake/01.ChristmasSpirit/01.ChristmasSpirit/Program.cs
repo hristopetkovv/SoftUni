bc4cceaf967c3891feb3f61fc838f81015b6513b
Hristo <i4kowee@gmail.com>
2019-08-08 18:04:56 +0300

    Added exercises

---
+++ Tech Module/MidExamRetake/01.ChristmasSpirit/01.ChristmasSpirit/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace _01.ChristmasSpirit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantity = int.Parse(Console.ReadLine());
+            int daysLeft = int.Parse(Console.ReadLine());
+            int ornamentSet = 0;
+            int treeSkirts = 0;
+            int treeGarlands = 0;
+            int treeLights = 0;
+            int spirit = 0;
+            int lastDay = daysLeft;
+
+            for (int i = 1; i <= daysLeft; i++)
+            {
+                if (i % 10 == 0)
+                {
+                    spirit -= 20;
+                    treeSkirts += 1;
+                    treeLights += 1;
+                    treeGarlands += 1;
+                }
+
+                if (i % 11 == 0)
+                {
+                    quantity += 2;
+                }
+                if (i % 3 == 0)
+                {
+                    treeSkirts += quantity;
+                    treeGarlands += quantity;
+                    spirit += 13;
+                }
+                if (i % 5 == 0)
+                {
+                    treeLights += quantity;
+                    spirit += 17;
+                    if (i % 3 == 0)
+                    {
+                        spirit += 30;
+                    }
+                }
+
+                if (i % 2 == 0)
+                {
+                    ornamentSet += quantity;
+                    spirit += 5;
+                }
+            }
+            if (lastDay % 10 == 0)
+            {
+                spirit -= 30;
+            }
+            int costOfOrnament = ornamentSet * 2;
+            int CostOftreeSkirts = treeSkirts * 5;
+            int costOftreeGarlands = treeGarlands * 3;
+            int costOftreeLights = treeLights * 15;
+            int totalCost = costOfOrnament + costOftreeGarlands + costOftreeLights + CostOftreeSkirts;
+            Console.WriteLine($"Total cost: {totalCost}");
+            Console.WriteLine($"Total spirit: {spirit}");
+        }
+    }
+}
---
+++ Tech Module/MidExamRetake/02.SantaList/02.SantaList/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.SantaList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = Console.ReadLine()
+                .Split("&")
+                .ToList();
+
+            string input = Console.ReadLine();
+
+            while (input != "Finished!")
+            {
+                string[] tokens = input.Split(" ");
+
+                string command = tokens[0];
+
+                string kidName = tokens[1];
+
+                if (command == "Bad")
+                {
+                    if (!list.Contains(kidName))
+                    {
+                        list.Insert(0, kidName);
+                    }
+                }
+
+                if (command == "Good")
+                {
+                    if (list.Contains(kidName))
+                    {
+                        list.Remove(kidName);
+                    }
+                }
+
+                if (command == "Rename")
+                {
+                    string newKid = tokens[2];
+                    if (list.Contains(kidName))
+                    {
+                        int index = list.IndexOf(kidName);
+                        list.Remove(kidName);
+                        list.Insert(index, newKid);
+                    }
+
+                }
+
+                if (command == "Rearrange")
+                {
+                    if (list.Contains(kidName))
+                    {
+                        list.Remove(kidName);
+                        list.Add(kidName);
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(", ", list));
+        }
+    }
+}
