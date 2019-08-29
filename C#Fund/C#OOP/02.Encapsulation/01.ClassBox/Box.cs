39c19d9e1dae2cf9099e602017dc684c9c7e1b90
Hristo <i4kowee@gmail.com>
2019-08-29 22:13:17 +0300

    Added exercises

---
+++ C#Fund/C#OOP/02.Encapsulation/01.ClassBox/Box.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.ClassBox
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Length
+        {
+            get => this.length;
+            private set
+            {
+                this.ValidateParameter(value, nameof(this.Length));
+                this.length = value;
+            }
+        }
+
+        public double Width
+        {
+            get => this.width;
+            private set
+            {
+                this.ValidateParameter(value, nameof(this.Width));
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get => this.height;
+            private set
+            {
+                this.ValidateParameter(value, nameof(this.Height));
+                this.height = value;
+            }
+        }
+
+
+        private void ValidateParameter(double value, string parameterName)
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException($"{parameterName} cannot be zero or negative");
+            }
+        }
+
+        public double CalculateSurfaceArea()
+        {
+            return 2 * (this.length * this.width) + this.CalculateLateralSurfaceArea();
+        }
+
+        public double CalculateLateralSurfaceArea()
+        {
+            return 2 * (this.length * this.height + this.width * this.height); 
+        }
+
+        public double CalculateVolume()
+        {
+            return this.width * this.height * this.length;
+        }
+    }
+}
---
+++ C#Fund/C#OOP/02.Encapsulation/01.ClassBox/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _01.ClassBox
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            try
+            {
+                Box box = new Box(length, width, height);
+
+                Console.WriteLine($"Surface Area - {box.CalculateSurfaceArea():f2}");
+                Console.WriteLine($"Lateral Surface Area - {box.CalculateLateralSurfaceArea():f2}");
+                Console.WriteLine($"Volume - {box.CalculateVolume():f2}");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
