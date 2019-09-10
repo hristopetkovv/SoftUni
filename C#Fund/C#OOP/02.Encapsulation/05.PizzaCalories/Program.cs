0fa14a6b4be0a682edc2c4007af261d14c56bc6f
Hristo <i4kowee@gmail.com>
2019-09-10 19:07:12 +0300

    Added exercise

--- C#Fund/C#OOP/02.Encapsulation/05.PizzaCalories/Program.cs
+++ C#Fund/C#OOP/02.Encapsulation/05.PizzaCalories/Program.cs
@@ -6,7 +6,44 @@ namespace _05.PizzaCalories
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                string[] pizzaArgs = Console.ReadLine().Split();
+
+                string pizzaName = pizzaArgs[1];
+
+                string[] doughArgs = Console.ReadLine().Split();
+
+                string doughFlourType = doughArgs[1];
+                string doughBakingTechnique = doughArgs[2];
+                double weight = double.Parse(doughArgs[3]);
+
+                Dough dough = new Dough(doughFlourType, doughBakingTechnique, weight);
+
+                Pizza pizza = new Pizza(pizzaName, dough);
+
+                string inputLine = Console.ReadLine();
+
+                while(inputLine != "END")
+                {
+                    string[] toppingArgs = inputLine.Split();
+
+                    string toppingType = toppingArgs[1];
+                    double weightTopping = double.Parse(toppingArgs[2]);
+
+                    Topping topping = new Topping(toppingType, weightTopping);
+
+                    pizza.AddTopping(topping);
+
+                    inputLine = Console.ReadLine();
+                }
+
+                Console.WriteLine($"{pizza.Name} - {pizza.GetTotalCalories().ToString("f2")} Calories.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
