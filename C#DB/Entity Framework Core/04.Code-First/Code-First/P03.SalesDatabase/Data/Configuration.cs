87462c7c73f460270ad383c28bcd7d23e987813b
Hristo <i4kowee@gmail.com>
2019-11-13 23:50:58 +0200

    Added exercises

---
+++ C#DB/Entity Framework Core/04.Code-First/Code-First/P03.SalesDatabase/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace P03.SalesDatabase.Data
+{
+    static class Configuration
+    {
+        public static string ConnectionString = @"Server=I4KOWE\SQLEXPRESS;Database=SalesDatabase;Integrated Security=true;";
+    }
+}
