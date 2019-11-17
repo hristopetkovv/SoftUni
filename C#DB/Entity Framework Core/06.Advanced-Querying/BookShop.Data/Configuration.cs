236ce56620f2762b55ad310c7994e9504b5d534c
Hristo <i4kowee@gmail.com>
2019-11-17 18:28:01 +0200

    Added exercises

---
+++ C#DB/Entity Framework Core/06.Advanced-Querying/BookShop.Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace BookShop.Data
+{
+    internal class Configuration
+    {
+        internal static string ConnectionString => @"Server=I4KOWE\SQLEXPRESS;Database=BookShop;Integrated Security=True;";
+    }
+}
---
+++ C#DB/Entity Framework Core/06.Advanced-Querying/BookShop/StartUp.cs
@@ -0,0 +1,70 @@
+﻿namespace BookShop
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using BookShop.Models;
+    using Data;
+
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (var db = new BookShopContext())
+            {
+                //DbInitializer.ResetDatabase(db);
+
+                string input = Console.ReadLine();
+
+                string result = GetBooksByCategory(db, input);
+
+                Console.WriteLine(result);
+                
+            }
+        }
+
+        //P01
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var books = context
+                .Books
+                .Where(b => b.AgeRestriction.ToString().ToLower() == command.ToLower())
+                .Select(b => new
+                {
+                    b.Title
+                })
+                .OrderBy(b => b.Title)
+                .ToList();
+
+            foreach (var b in books)
+            {
+                sb.AppendLine(b.Title);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        //P05
+        public static string GetBooksByCategory(BookShopContext context, string input)
+        {
+            string[] categories = input
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(c => c.ToLower())
+                .ToArray();
+
+            var books = context
+                .Books
+                .Where(b => b.BookCategories.Any(bc => categories.Contains(bc.Category.Name.ToLower())))
+                .Select(b => b.Title)
+                .OrderBy(b => b)
+                .ToList();
+
+            return string.Join(Environment.NewLine, books);
+        }
+    }
+}
