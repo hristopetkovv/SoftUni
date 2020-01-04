83d5f7da9b510aa1a18902cd1fa2e337baca54b3
Hristo <i4kowee@gmail.com>
2020-01-04 21:43:51 +0200

    Added Exam Models

---
+++ C#DB/Entity Framework Core/Exam/TeisterMask/Data/Models/EmployeeTask.cs
@@ -0,0 +1,20 @@
+﻿namespace TeisterMask.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class EmployeeTask
+    {
+        [Required]
+        [ForeignKey("Employee")]
+        public int EmployeeId { get; set; }
+
+        public Employee Employee { get; set; }
+
+        [Required]
+        [ForeignKey("Task")]
+        public int TaskId { get; set; }
+
+        public Task Task { get; set; }
+    }
+}
