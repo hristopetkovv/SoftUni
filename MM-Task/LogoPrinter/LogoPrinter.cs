7007d55c8a1e31aa7d59c23e613c687f530c8807
Hristo <i4kowee@gmail.com>
2020-02-20 10:26:02 +0200

    MM Logo Task

---
+++ MM-Task/LogoPrinter/LogoPrinter.cs
@@ -0,0 +1,119 @@
+﻿namespace LogoPrinter
+{
+    using System;
+
+    public class LogoPrinter
+    {
+        private int figureSize;
+        private int leftRightDashesCount;
+        private int middleRowIndex;
+        private int starsCount;
+        private int dashesCount;
+        private int midDashesCount;
+        private int starsInBottomMiddle;
+        private int dashesInBottomMiddle;
+
+        public LogoPrinter(int figureSize, int middleRowIndex, int leftRightDashesCount, int starsCount, int dashesCount, int midDashesCount)
+        {
+            this.FigureSize = figureSize;
+            this.MiddleRowIndex = middleRowIndex;
+            this.LeftRightDashesCount = leftRightDashesCount;
+            this.StarsCount = starsCount;
+            this.DashesCount = dashesCount;
+            this.MidDashesCount = midDashesCount;
+        }
+
+        public int FigureSize
+        {
+            get { return this.figureSize; }
+            set { this.figureSize = value; }
+        }
+        public int LeftRightDashesCount
+        {
+            get { return this.leftRightDashesCount; }
+            set { this.leftRightDashesCount = value; }
+        }
+        public int MiddleRowIndex
+        {
+            get { return this.middleRowIndex; }
+            set { this.middleRowIndex = value; }
+        }
+        public int StarsCount
+        {
+            get { return this.starsCount; }
+            set { this.starsCount = value; }
+        }
+        public int DashesCount
+        {
+            get { return this.dashesCount; }
+            set { this.dashesCount = value; }
+        }
+        public int MidDashesCount
+        {
+            get { return this.midDashesCount; }
+            set { this.midDashesCount = value; }
+        }
+        public int StarsInBottomMiddle
+        {
+            get { return this.starsInBottomMiddle; }
+            set { this.starsInBottomMiddle = value; }
+        }
+        public int DashesInBottomMiddle
+        {
+            get { return this.dashesInBottomMiddle; }
+            set { this.dashesInBottomMiddle = value; }
+        }
+
+
+        public void PrintUpperPartOfLogo()
+        {
+            for (int i = 0; i <= this.MiddleRowIndex; i++)
+            {
+                Console.WriteLine(
+                    new string('-', this.LeftRightDashesCount)
+                    + new string('*', this.StarsCount)
+                    + new string('-', this.DashesCount)
+                    + new string('*', this.StarsCount)
+                    + new string('-', this.MidDashesCount)
+                    + new string('*', this.StarsCount)
+                    + new string('-', this.DashesCount)
+                    + new string('*', this.StarsCount)
+                    + new string('-', this.LeftRightDashesCount));
+                this.LeftRightDashesCount--;
+                this.StarsCount += 2;
+                this.DashesCount -= 2;
+                this.MidDashesCount -= 2;
+            }
+        }
+
+        public void PrintBottomPartOfLogo(int middleDashesCount)
+        {
+            this.StarsCount = this.FigureSize;
+            this.StarsInBottomMiddle = this.FigureSize * 2 - 1;
+            this.DashesInBottomMiddle = middleDashesCount;
+            this.DashesCount = 1;
+
+            for (int i = 0; i <= this.MiddleRowIndex; i++)
+            {
+                Console.WriteLine(
+                     new string('-', this.LeftRightDashesCount)
+                     + new string('*', this.StarsCount)
+                     + new string('-', this.DashesCount)
+                     + new string('*', this.StarsInBottomMiddle)
+                     + new string('-', this.DashesCount)
+                     + new string('*', this.StarsCount)
+                     + new string('-', this.DashesInBottomMiddle)
+                     + new string('*', this.StarsCount)
+                     + new string('-', this.DashesCount)
+                     + new string('*', this.StarsInBottomMiddle)
+                     + new string('-', this.DashesCount)
+                     + new string('*', this.StarsCount)
+                     + new string('-', this.LeftRightDashesCount));
+                this.LeftRightDashesCount--;
+                this.DashesCount += 2;
+                this.StarsInBottomMiddle -= 2;
+                this.DashesInBottomMiddle -= 2;
+            }
+        }
+    }
+}
---
+++ MM-Task/LogoPrinter/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace LogoPrinter
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Please enter and odd number between 2 and 10 000: ");
+            var figureSize = int.Parse(Console.ReadLine());
+
+            var mid = figureSize * 2;
+            var leftRightDasheshCount = figureSize;
+            var middleRowIndex = figureSize / 2;
+            var starsCount = figureSize;
+            var dashesCount = figureSize;
+
+            var logoPrinter = new LogoPrinter(figureSize, middleRowIndex, leftRightDasheshCount, starsCount, dashesCount, mid);
+
+            logoPrinter.PrintUpperPartOfLogo();
+
+            logoPrinter.PrintBottomPartOfLogo(logoPrinter.MidDashesCount);
+
+        }
+    }
+}
+
