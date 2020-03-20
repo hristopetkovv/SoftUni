a9a75da7dc0ecef82521eac8626ad370f945af91
Hristo <i4kowee@gmail.com>
2020-03-20 11:14:37 +0200

    Delete

--- C#WEB/ASP.NET Core/AnimalsShop/AnimalsShop/Data/Data.Models/DataValidation.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AnimalsShop.Data.Data.Models
-{
-    public class DataValidation
-    {
-        public const int NameMaxLength = 25;
-        public const int DescriptionMaxLength = 1000;
-    }
-}
