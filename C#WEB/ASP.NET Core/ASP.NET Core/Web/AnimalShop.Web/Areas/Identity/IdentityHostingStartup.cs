83a3ab27c891c41414cccb2332f01ae09993ef4d
Hristo <i4kowee@gmail.com>
2020-03-21 17:00:23 +0200

    Add PetAdvice Controller

---
+++ C#WEB/ASP.NET Core/ASP.NET Core/Web/AnimalShop.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AnimalShop.Data;
+using AnimalShop.Data.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AnimalShop.Web.Areas.Identity.IdentityHostingStartup))]
+namespace AnimalShop.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ C#WEB/ASP.NET Core/ASP.NET Core/Web/AnimalShop.Web/Controllers/PetAdvicesController.cs
@@ -0,0 +1,32 @@
+﻿namespace AnimalShop.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class PetAdvicesController : Controller
+    {
+        public IActionResult Dog()
+        {
+            return this.View();
+        }
+
+        public IActionResult Cat()
+        {
+            return this.View();
+        }
+
+        public IActionResult Bird()
+        {
+            return this.View();
+        }
+
+        public IActionResult Fish()
+        {
+            return this.View();
+        }
+
+        public IActionResult SmallPet()
+        {
+            return this.View();
+        }
+    }
+}
