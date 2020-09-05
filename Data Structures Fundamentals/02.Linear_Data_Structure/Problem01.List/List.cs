000f0e5e0808ecc087066ad5a0f326f8a7c4c0b0
Hristo <i4kowee@gmail.com>
2020-09-05 22:30:42 +0300

    Implement Abstract Data Structure methods
    Implement List, Stack, Queue and Linked List methods

---
+++ Data Structures Fundamentals/02.Linear_Data_Structure/Problem01.List/List.cs
@@ -0,0 +1,146 @@
+﻿namespace Problem01.List
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class List<T> : IAbstractList<T>
+    {
+        private const int DEFAULT_CAPACITY = 4;
+        private T[] _items;
+
+        public List(int capacity = DEFAULT_CAPACITY)
+        {
+            if (capacity < 0)
+            {
+                throw new IndexOutOfRangeException($"{capacity} is not a valid capacity");
+            }
+
+            this._items = new T[capacity];
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                this.ValidateIndex(index);
+                return this._items[index];
+            }
+            set
+            {
+                this.ValidateIndex(index);
+                this._items[index] = value;
+            }
+        }
+
+        public int Count { get; private set; }
+
+        public void Add(T item)
+        {
+            this.EnsureNotEmpty();
+            this._items[this.Count++] = item;
+        }
+
+     
+
+        public bool Contains(T item)
+        {
+            return this.IndexOf(item) != -1;
+        }
+
+
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (this._items[i].Equals(item))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public void Insert(int index, T item)
+        {
+            this.ValidateIndex(index);
+            this.EnsureNotEmpty();
+
+            for (int i = this.Count; i > index; i--)
+            {
+                this._items[i] = this._items[i - 1];
+            }
+
+            this._items[index] = item;
+            this.Count++;
+        }
+
+      
+        public bool Remove(T item)
+        {
+            int indexOfElement = this.IndexOf(item);
+            
+            if (indexOfElement == -1)
+            {
+                return false;
+            }
+
+            this.RemoveAt(indexOfElement);
+
+            return true;
+        }
+
+        public void RemoveAt(int index)
+        {
+            this.ValidateIndex(index);
+
+            for (int i = index; i < this.Count - 1; i++)
+            {
+                this._items[i] = this._items[i + 1];
+            }
+
+            this._items[this.Count - 1] = default;
+            this.Count--;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                yield return this._items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() 
+            => this.GetEnumerator();
+
+        private void EnsureNotEmpty()
+        {
+            if (this.Count == this._items.Length)
+            {
+                this.Resize();
+            }
+        }
+
+        private void Resize()
+        {
+            var newArr = new T[this._items.Length * 2];
+            for (int i = 0; i < this._items.Length; i++)
+            {
+                newArr[i] = this._items[i];
+            }
+
+            this._items = newArr;
+        }
+
+        private void ValidateIndex(int index)
+        {
+            if (index < 0 || index >= this.Count)
+            {
+                throw new IndexOutOfRangeException(nameof(index));
+            }
+        }
+
+    }
+}
---
+++ Data Structures Fundamentals/02.Linear_Data_Structure/Problem04.SinglyLinkedList/SinglyLinkedList.cs
@@ -0,0 +1,115 @@
+﻿namespace Problem04.SinglyLinkedList
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class SinglyLinkedList<T> : IAbstractLinkedList<T>
+    {
+        private Node<T> _head;
+
+        public SinglyLinkedList()
+        {
+            this._head = null;
+            this.Count = 0;
+        }
+
+        public SinglyLinkedList(Node<T> item)
+        {
+            this._head = item;
+            this.Count = 1;
+        }
+
+        public int Count { get; private set; }
+
+        public void AddFirst(T item)
+        {
+            Node<T> newNode = new Node<T>(item, this._head);
+            this._head = newNode;
+            this.Count++;
+        }
+
+        public void AddLast(T item)
+        {
+            Node<T> newNode = new Node<T>(item);
+            Node<T> current = this._head;
+
+            if (current == null)
+            {
+                this._head = newNode;
+            }
+            else
+            {
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+
+                current.Next = newNode;
+            }
+            this.Count++;
+        }
+
+        public T GetFirst()
+        {
+            this.ValidateIfListIsNotEmpty();
+
+            return this._head.Value;
+        }
+
+       
+
+        public T GetLast()
+        {
+            this.ValidateIfListIsNotEmpty();
+            Node<T> current = this._head;
+
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+
+            return current.Value;
+        }
+
+        public T RemoveFirst()
+        {
+            this.ValidateIfListIsNotEmpty();
+
+            Node<T> firstNode = this._head;
+
+            this._head = this._head.Next;
+            this.Count--;
+
+            return firstNode.Value;
+        }
+
+        //public T RemoveLast()
+        //{
+        //    this.ValidateIfListIsNotEmpty();
+        //}
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node<T> current = this._head;
+
+            while (current != null)
+            {
+                yield return current.Value;
+
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => this.GetEnumerator();
+
+        private void ValidateIfListIsNotEmpty()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("Linked list is empty!");
+            }
+        }
+    }
+}
