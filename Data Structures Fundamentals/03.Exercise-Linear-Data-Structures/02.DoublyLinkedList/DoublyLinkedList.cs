f59b24b6e81509578aead97bd65df28309fa21a3
Hristo <i4kowee@gmail.com>
2020-09-14 18:27:45 +0300

    Implement DoublyLinkedList methods
    from O(n) to O(1) complexity

--- Data Structures Fundamentals/03.Exercise-Linear-Data-Structures/02.DoublyLinkedList/DoublyLinkedList.cs
+++ Data Structures Fundamentals/03.Exercise-Linear-Data-Structures/02.DoublyLinkedList/DoublyLinkedList.cs
@@ -7,47 +7,140 @@
     public class DoublyLinkedList<T> : IAbstractLinkedList<T>
     {
         private Node<T> head;
+        private Node<T> tail;
+
+        public DoublyLinkedList()
+        {
+            this.head = this.tail = null;
+            this.Count = 0;
+        }
+
+        public DoublyLinkedList(Node<T> head)
+        {
+            this.head = this.tail = head;
+            this.Count = 1;
+        }
 
         public int Count { get; private set; }
 
         public void AddFirst(T item)
         {
-            throw new NotImplementedException();
+            Node<T> toInsert = new Node<T>
+            { 
+                Item = item,
+            };
+
+            if (this.Count == 0)
+            {
+                this.head = this.tail = toInsert;
+            }
+            else
+            {
+                this.head.Previous = toInsert;
+                toInsert.Next = this.head;
+                this.head = toInsert;
+            }
+
+            this.Count++;
         }
 
         public void AddLast(T item)
         {
-            throw new NotImplementedException();
+            Node<T> toInsert = new Node<T>
+            { 
+                Item = item
+            };
+
+            if (this.Count == 0)
+            {
+                this.head = this.tail = toInsert;
+            }
+            else
+            {
+                toInsert.Previous = this.tail;
+                this.tail.Next = toInsert;
+                this.tail = toInsert;
+            }
+
+            this.Count++;
         }
 
         public T GetFirst()
         {
-            throw new NotImplementedException();
+            this.EnsureNotEmpty();
+
+            return this.head.Item;
         }
 
         public T GetLast()
         {
-            throw new NotImplementedException();
+            this.EnsureNotEmpty();
+
+            return this.tail.Item;
         }
 
         public T RemoveFirst()
         {
-            throw new NotImplementedException();
+            this.EnsureNotEmpty();
+
+            Node<T> current = this.head;
+
+            if (this.Count == 1)
+            {
+                this.head = this.tail = null;
+            }
+            else
+            {
+                Node<T> newHead = this.head.Next;
+                newHead.Previous = null;
+                this.head = newHead;
+            }
+
+            this.Count--;
+
+            return current.Item;
         }
 
         public T RemoveLast()
         {
-            throw new NotImplementedException();
+            this.EnsureNotEmpty();
+
+            Node<T> current = this.tail;
+
+            if (this.Count == 1)
+            {
+                this.head = this.tail = null;
+            }
+            else
+            {
+                Node<T> newTail = this.tail.Previous;
+                newTail.Next = null;
+                this.tail = newTail;
+            }
+
+            this.Count--;
+
+            return current.Item;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            Node<T> current = this.head;
+
+            while (current != null)
+            {
+                yield return current.Item;
+                current = current.Next;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
+            => this.GetEnumerator();
+
+        private void EnsureNotEmpty()
         {
-            throw new NotImplementedException();
+            if (this.Count == 0)
+                throw new InvalidOperationException();
         }
     }
 }
