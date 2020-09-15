4c3b48b344ac3463a046a423278cc8f29ac72fba
Hristo <i4kowee@gmail.com>
2020-09-15 17:27:36 +0300

    Balanced  Parentheses

--- Data Structures Fundamentals/03.Exercise-Linear-Data-Structures/04.BalancedParentheses/BalancedParenthesesSolve.cs
+++ Data Structures Fundamentals/03.Exercise-Linear-Data-Structures/04.BalancedParentheses/BalancedParenthesesSolve.cs
@@ -7,7 +7,40 @@
     {
         public bool AreBalanced(string parentheses)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(parentheses) || parentheses.Length % 2 == 1)
+            {
+                return false;
+            }
+
+            Stack<char> openBrackets = new Stack<char>(parentheses.Length / 2);
+
+            foreach (char currentBracket in parentheses)
+            {
+                char expectedCharacter = default;
+
+                switch (currentBracket)
+                {
+                    case ')':
+                        expectedCharacter = '(';
+                        break;
+                    case ']':
+                        expectedCharacter = '[';
+                        break;
+                    case '}':
+                        expectedCharacter = '{';
+                        break;
+                    default:
+                        openBrackets.Push(currentBracket);
+                        break;
+                }
+
+                if (expectedCharacter != default && openBrackets.Pop() != expectedCharacter)
+                {
+                    return false;
+                }
+            }
+
+            return openBrackets.Count == 0;
         }
     }
 }
