17b7ce1b7a7272f46dae9639961e31e42a46dfe6
Hristo <i4kowee@gmail.com>
2020-09-15 17:06:47 +0300

    Implement ReversedList

--- Data Structures Fundamentals/03.Exercise-Linear-Data-Structures/03.ReversedList/ReversedList.cs
+++ Data Structures Fundamentals/03.Exercise-Linear-Data-Structures/03.ReversedList/ReversedList.cs
@@ -25,11 +25,13 @@
         {
             get
             {
-                throw new NotImplementedException();
+                this.ValidateIndex(index);
+                return this._items[this.Count - 1 - index];
             }
             set
             {
-                throw new NotImplementedException();
+                this.ValidateIndex(index);
+                this._items[index] = value;
             }
         }
 
@@ -37,42 +39,106 @@
 
         public void Add(T item)
         {
-            throw new NotImplementedException();
+            this.GrowIfNeccessary();
+            this._items[this.Count++] = item;
         }
+
+       
 
         public bool Contains(T item)
         {
-            throw new NotImplementedException();
+            return this.IndexOf(item) != -1;
         }
 
         public int IndexOf(T item)
         {
-            throw new NotImplementedException();
+            for (int i = 1; i <= this.Count; i++)
+            {
+                if (this._items[this.Count - i].Equals(item))
+                {
+                    return i - 1;
+                }
+            }
+
+            return -1;
         }
 
         public void Insert(int index, T item)
         {
-            throw new NotImplementedException();
+            this.GrowIfNeccessary();
+            this.ValidateIndex(index);
+            int indexToInsert = this.Count - index;
+
+            for (int i = this.Count; i > indexToInsert; i--)
+            {
+                this._items[i] = this._items[i - 1];
+            }
+
+            this._items[indexToInsert] = item;
+            this.Count++;
         }
 
         public bool Remove(T item)
         {
-            throw new NotImplementedException();
+            int indexOfElement = this.IndexOf(item);
+
+            if (indexOfElement == -1)
+            {
+                return false;
+            }
+
+            this.RemoveAt(indexOfElement);
+            return false;
         }
 
         public void RemoveAt(int index)
         {
-            throw new NotImplementedException();
+            this.ValidateIndex(index);
+            int indexOfElement = this.Count - 1 - index;
+
+            for (int i = indexOfElement; i < this.Count - 1; i++)
+            {
+                this._items[i] = this._items[i + 1];
+            }
+            this._items[this.Count - 1] = default;
+            this.Count--;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            for (int i = this.Count - 1; i >= 0; i--)
+            {
+                yield return this._items[i];
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return this.GetEnumerator();
+        }
+
+        private void GrowIfNeccessary()
+        {
+            if (this.Count == this._items.Length)
+            {
+                this.Grow();
+            }
+        }
+
+        private void Grow()
+        {
+            T[] newItems = new T[this._items.Length * 2];
+            Array.Copy(this._items, newItems, this._items.Length);
+
+            this._items = newItems;
+        }
+
+        private void ValidateIndex(int index)
+        {
+            if (index < 0 || index >= this.Count)
+            {
+                throw new IndexOutOfRangeException("Index is out of range");
+            }
         }
     }
 }
