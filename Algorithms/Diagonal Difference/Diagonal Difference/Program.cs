f276e7e289995f2d0a13f5e31d0f507e13c25222
Hristo <i4kowee@gmail.com>
2020-09-26 16:51:44 +0300

    Diagonal Difference task

---
+++ Algorithms/Diagonal Difference/Diagonal Difference/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Diagonal_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] arr = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                string[] splitedInput = input.Split();
+                for (int j = 0; j < n; j++)
+                {
+                    arr[i, j] = int.Parse(splitedInput[j]);
+                }
+            }
+
+            int firstDiagSum = 0;
+            int secondDiagSum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                firstDiagSum += Convert.ToInt32(arr[i, i]);
+            }
+            for (int i = 0; i < n; i++)
+            {
+                secondDiagSum += Convert.ToInt32(arr[((n - 1) - i), i]);
+            }
+            Console.WriteLine(Math.Abs(firstDiagSum-secondDiagSum));
+        }
+    }
+}
