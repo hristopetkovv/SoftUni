3034dbde05b016993a21e755031b32bb91399a27
Hristo <i4kowee@gmail.com>
2020-09-26 17:08:10 +0300

    Plus Minus array task

---
+++ Algorithms/Plus Minus/Plus Minus/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace Plus_Minus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+            int positiveCount = 0;
+            int negativeCount = 0;
+            int zeroCount = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int num = arr[i];
+                if (num > 0)
+                {
+                    positiveCount++;
+                }
+                else if (num < 0)
+                {
+                    negativeCount++;
+                }
+                else
+                {
+                    zeroCount++;
+                }
+            }
+
+            Console.WriteLine(String.Format("{0:0.000000}", (double)positiveCount / arr.Length));
+            Console.WriteLine(String.Format("{0:0.000000}", (double)negativeCount / arr.Length));
+            Console.WriteLine(String.Format("{0:0.000000}", (double)zeroCount / arr.Length));
+        }
+    }
+}
