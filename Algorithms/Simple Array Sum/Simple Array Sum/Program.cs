5b6657ace5fe3f28a4f3ca969047847d11cee7fb
Hristo <i4kowee@gmail.com>
2020-09-26 16:23:23 +0300

    Add simple array sum task

---
+++ Algorithms/Simple Array Sum/Simple Array Sum/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace Simple_Array_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+            Console.WriteLine(arr.Sum());
+        }
+    }
+}
