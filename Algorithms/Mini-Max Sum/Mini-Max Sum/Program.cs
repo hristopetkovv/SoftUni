d33737ade8a1c4243fef22b45d0c4584f92a0529
Hristo <i4kowee@gmail.com>
2020-09-28 18:04:31 +0300

    Mini-Max sum task

---
+++ Algorithms/Mini-Max Sum/Mini-Max Sum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mini_Max_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long[] arr = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(long.Parse)
+                .ToArray();
+
+            long max = arr.Sum();
+            long min = arr.Sum();
+
+            max -= arr.Min();
+            min -= arr.Max();
+            Console.WriteLine(min + " " + max);
+        }
+    }
+}
