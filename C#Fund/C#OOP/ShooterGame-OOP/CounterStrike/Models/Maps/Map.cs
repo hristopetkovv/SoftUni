d974f7493f9bbd24d36f7bcf10315bd2eecf1b3c
Hristo <i4kowee@gmail.com>
2020-10-06 17:04:54 +0300

    ShooterGame OOP
    Structure of the project

---
+++ C#Fund/C#OOP/ShooterGame-OOP/CounterStrike/Models/Maps/Map.cs
@@ -0,0 +1,74 @@
+﻿using CounterStrike.Models.Maps.Contracts;
+using CounterStrike.Models.Players;
+using CounterStrike.Models.Players.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CounterStrike.Models.Maps
+{
+    public class Map : IMap
+    {
+        public string Start(ICollection<IPlayer> players)
+        {
+            var terrorists = players
+                .Where(t => t.GetType() == typeof(Terrorist))
+                .ToList();
+
+            var counterTerrorists = players
+                .Where(ct => ct.GetType() == typeof(CounterTerrorist))
+                .ToList();
+
+            while (terrorists.Any(x => x.IsAlive) && counterTerrorists.Any(x => x.IsAlive))
+            {
+                foreach (var terro in terrorists.Where(t => t.IsAlive))
+                {
+                    if (!terro.IsAlive)
+                    {
+                        continue;
+                    }
+
+                    foreach (var counterTerro in counterTerrorists.Where(ct => ct.IsAlive))
+                    {
+                        if (!counterTerro.IsAlive)
+                        {
+                            continue;
+                        }
+
+                        counterTerro.TakeDamage(terro.Gun.Fire());
+                    }
+                }
+
+                foreach (var counterTerro in counterTerrorists.Where(ct => ct.IsAlive))
+                {
+                    if (!counterTerro.IsAlive)
+                    {
+                        continue;
+                    }
+
+                    foreach (var terro in terrorists.Where(t => t.IsAlive))
+                    {
+                        if (!terro.IsAlive)
+                        {
+                            continue;
+                        }
+
+                        terro.TakeDamage(counterTerro.Gun.Fire());
+                    }
+                }
+            }
+
+            string result = string.Empty;
+
+            if (terrorists.Any(t => t.IsAlive))
+            {
+                result = "Terrorist wins!";
+            }
+            else
+            {
+                result = "Counter Terrorist wins!";
+            }
+
+            return result;
+        }
+    }
+}
---
+++ C#Fund/C#OOP/ShooterGame-OOP/CounterStrike/Repositories/PlayerRepository.cs
@@ -0,0 +1,42 @@
+﻿using CounterStrike.Models.Players.Contracts;
+using CounterStrike.Repositories.Contracts;
+using CounterStrike.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CounterStrike.Repositories
+{
+    public class PlayerRepository : IRepository<IPlayer>
+    {
+        private readonly List<IPlayer> players;
+
+        public PlayerRepository()
+        {
+            this.players = new List<IPlayer>();
+        }
+
+        public IReadOnlyCollection<IPlayer> Models
+            => this.players;
+
+        public void Add(IPlayer model)
+        {
+            if (model == null)
+            {
+                throw new ArgumentException(ExceptionMessages.InvalidPlayerRepository);
+            }
+
+            this.players.Add(model);
+        }
+
+        public IPlayer FindByName(string name)
+        {
+            return this.players.FirstOrDefault(x => x.Username == name);
+        }
+
+        public bool Remove(IPlayer model)
+        {
+            return players.Remove(model);
+        }
+    }
+}
